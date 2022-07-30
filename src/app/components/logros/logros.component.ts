134c91704a760f17217c59b5fe8aa2da9d484f86
NahuelGinzo <70040659+NahuelGinzo@users.noreply.github.com>
2022-07-30 03:05:00 -0300

    nuevo proyecto

--- src/app/components/logros/logros.component.ts
+++ src/app/components/logros/logros.component.ts
@@ -10,10 +10,14 @@ export class LogrosComponent implements OnInit {
   constructor(private datosPorfolio: PorfolioService) { }
 proyectList: any;
 proyect1List: any;
+proyect2List: any;
+proyect3List: any;
   ngOnInit(): void {
     this.datosPorfolio.obtnerDatos().subscribe(data => {
       this.proyectList = data.proyect;
 this.proyect1List = data.proyect1;
+this.proyect2List = data.proyect2;
+this.proyect3List = data.proyect3;
     })
   }
 
